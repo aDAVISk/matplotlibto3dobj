5ae91a1f671d6334ef5bc4e4f1a572cde721781d
aDAVISk <akito.davis@gmail.com>
2023-12-30 19:25:50 +0900

    Successfully create and save collada.Lineset
    The generated file can be loaded on Blender successfully.
    #5

--- proto/try_pycollada.py
+++ proto/try_pycollada.py
@@ -1,13 +1,15 @@
 # https://pycollada.readthedocs.io/en/latest/creating.html
 # https://github.com/pycollada/pycollada/blob/master/collada/tests/test_lineset.py
 
-import collada
-
-from collada import *
 import numpy as np
 import pprint
 ppbase = pprint.PrettyPrinter(indent=3)
 pp = lambda x: ppbase.pprint(x)
+
+
+import collada
+from collada.xmlutil import etree
+tostring = etree.tostring
 
 obj = collada.Collada(validate_output=True)
 #pp(obj) #=> <Collada geometries=0>
@@ -25,15 +27,36 @@ lineset = geom.createLineSet(indices, input_list, "mymaterial")
 
 # Put all the data to the internal xml node (xmlnode) so it can be serialized.
 # https://pycollada.github.io/reference/generated/collada.DaeObject.html#collada.DaeObject.save
-lineset.save()
+#lineset.save()
+#pp(lineset.xmlnode) #=> <Element '{http://www.collada.org/2005/11/COLLADASchema}lines' at 0x0000014EF3A94680>
+#xmltxt = tostring(lineset.xmlnode)
+#pp(xmltxt) #=> (b'<lines xmlns="http://www.collada.org/2005/11/COLLADASchema" count="5" materi'\n b'al="mymaterial"><input offset="0" semantic="VERTEX" source="#mylinevertsourc'\n b'e" /><p>0 1 1 2 2 3 3 4 4 5</p></lines>')
 
-#effect = material.Effect("effect0", [], "phong", diffuse=(1,0,0), specular=(0,1,0))
+geom.primitives.append(lineset)
+obj.geometries.append(geom)
+
+
+
+effect = collada.material.Effect("effect0", [], "phong", diffuse=(1,0,0), specular=(0,1,0))
 #pp(effect) #=> <Effect id=effect0 type=phong>
-
 #pp(material) #=> <module 'collada.material' from 'C:\\Programs\\anaconda3\\envs\\py393D\\lib\\site-packages\\collada\\material.py'>
+obj.effects.append(effect)
 
-#mat = material.Material("material0", "mymaterial", effect)
+mat = collada.material.Material("material0", "mymaterial", effect)
 #pp(mat) #=> <Material id=material0 effect=effect0>
+obj.materials.append(mat)
+
+
+matnode = collada.scene.MaterialNode("materialref", mat, inputs=[])
+geomnode = collada.scene.GeometryNode(geom, [matnode])
+node = collada.scene.Node("node0", children=[geomnode])
+myscene = collada.scene.Scene("myscene", [node])
+obj.scenes.append(myscene)
+obj.scene = myscene
+
+obj.write("proto/tmp/testline.dae")
+
+
 #obj.effects.append(effect)
 #obj.materials.append(mat)
 #pp(obj) #=> <Collada geometries=0>
